844b0aac1d1074711a4e4438caa3aa294152c8de
gssim-kr <cremazer@gmail.com>
2022-01-23 17:18:24 +0900

    react-testing-library 실습

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
 });
